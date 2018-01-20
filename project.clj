ccc6edf8fd7a4fbe17c9ee23bc41fecae12a24df
dongwoo <dongwoo.kim@nhnent.com>
2018-01-20 15:26:46 +0900

    chore: update depencencies

--- project.clj
+++ project.clj
@@ -1,16 +1,10 @@
 (defproject todoapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :min-lein-version "2.7.1"
-
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.908"]
                  [org.clojure/core.async  "0.3.443"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.1"]
+                 [reagent "0.8.0-alpha2"]
+                 [re-frame "0.10.3-rc2"]
                  [bidi "2.1.2"]
                  [kibu/pushy "0.3.8"]]
 
@@ -22,79 +16,34 @@
   :cljsbuild {:builds
               [{:id "dev"
                 :source-paths ["src"]
-                ; :figwheel {:on-jsload "todoapp.core/main"
-                ;            :open-urls ["http://localhost:3449/index.html"]}
-
+                :figwheel {:on-jsload "todoapp.core/main"
+                           :open-urls ["http://localhost:3449/index.html"]}
                 :compiler {:main todoapp.core
                            :asset-path "js/compiled/out"
                            :output-to "resources/public/js/compiled/todoapp.js"
                            :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
-                           :preloads [devtools.preload]}}
-               ;; This next build is a compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
+                           :closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}
+                           :preloads [devtools.preload day8.re-frame.trace.preload]}}
                {:id "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/todoapp.js"
+                :compiler {:output-to "resources/public/js/compiled/todoapp.min.js"
                            :main todoapp.core
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
+  :figwheel {:http-server-root "public"
+             :server-port 3449
+             :server-ip "127.0.0.1"
+             :css-dirs ["resources/public/css"]
              :nrepl-port 7888}
 
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-
-             ;; doesn't work for you just run your own server :) (see lein-ring)
-
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-
-             ;; to pipe all the output to the repl
-             ;; :server-logfile false
-
-
-
-  ;; Setting up nREPL for Figwheel and ClojureScript dev
-  ;; Please see:
-  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
                                   [figwheel-sidecar "0.5.13"]
                                   [com.cemerick/piggieback "0.2.2"]
+                                  [day8.re-frame/trace "0.1.14"]
                                   [proto-repl "0.3.1"]]
-                   ;; need to add dev source path here to get user.clj loaded
                    :source-paths ["src" "dev"]
-                   ;; for CIDER
-                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-                   ;; need to add the compliled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
